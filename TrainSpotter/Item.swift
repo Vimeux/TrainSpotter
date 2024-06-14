abfcd66f2fc740967b78be54f1320df6faf96836
Benjamin Vimeux <vimeux.benji@gmail.com>
2024-06-14 19:06:43 +0200

    Initial Commit

---
+++ TrainSpotter/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TrainSpotter
+//
+//  Created by Benjamin Vimeux on 14/06/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
